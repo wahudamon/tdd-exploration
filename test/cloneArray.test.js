15326258308a9d366e61f8985c317c4f599934f7
Afwa Bagas Wahuda <afwa.bagas.1999@gmail.com>
2022-09-26 13:57:50 +0700

    feat: add test case for clone array function

--- test/cloneArray.test.js
+++ test/cloneArray.test.js
@@ -8,5 +8,6 @@ describe("Clone Array Tests", () => {
   it("properly clones array", () => {
     let array = [1, 2, 3];
     expect(cloneArray(array)).toEqual(array);
+    expect(cloneArray(array)).not.toBe(array);
   });
 });
