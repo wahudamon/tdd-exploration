397282e04f5d9116cbcca9c19db3b2501fd6557e
Afwa Bagas Wahuda <afwa.bagas.1999@gmail.com>
2022-09-26 12:46:24 +0700

    feat: add second test case

---
+++ test/stack.test.js
@@ -0,0 +1,35 @@
+class Stack {
+  constructor() {
+    this.top = -1;
+    this.items = {};
+  }
+
+  get peek() {
+    return this.items[this.top];
+  }
+
+  push(value) {
+    this.top += 1;
+    this.items[this.top] = value;
+  }
+}
+
+describe("My Stack", () => {
+  let stack;
+
+  beforeEach(() => {
+    stack = new Stack();
+  });
+
+  it("is created empty", () => {
+    expect(stack.top).toBe(-1);
+    expect(stack.items).toEqual({});
+  });
+
+  it("can push to the top", () => {
+    stack.push("avocado");
+    expect(stack.top).toBe(0);
+    expect(stack.peek).toBe("avocado");
+  });
+  it.todo("can pop off");
+});
